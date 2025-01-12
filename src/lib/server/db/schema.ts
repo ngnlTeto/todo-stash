e4e177a507f6ddc142d4ba8be9f9f5d4a0d41881
Henry <henry@gedert.de>
2025-01-12 21:19:01 +0100

    - added shad-cn - basic layouting

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,11 @@
 import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 
 export const user = sqliteTable('user', {
-	id: integer('id').primaryKey(),
-	age: integer('age')
+	id: integer('id').primaryKey({ autoIncrement: true }),
+	name: text('name'),
+	age: integer('age'),
+	password: text("password").notNull(),
 });
+
+
+export type NewUser = typeof user.$inferInsert;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,51 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { buttonVariants } from '$lib/components/ui/button';
+	import { Layers, Menu } from 'lucide-svelte';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+	import * as Sheet from '$lib/components/ui/sheet/index.js';
+	import LightSwitch from '$lib/components/light-switch.svelte';
+	import TagMenu from './tag-menu.svelte';
+</script>
+
+<div class="mx-4 flex h-14 flex-row content-center items-center">
+	<Sheet.Root>
+		<Sheet.Trigger
+			class="{buttonVariants({ variant: 'outline', size: 'icon' })}"
+		>
+			<Menu />
+		</Sheet.Trigger>
+
+		<Sheet.Content side="left">
+			<Sheet.Header>
+				<Sheet.Title>Tag Menu</Sheet.Title>
+				<Sheet.Description>Manage your tags and filter options</Sheet.Description>
+			</Sheet.Header>
+			<TagMenu />
+		</Sheet.Content>
+	</Sheet.Root>
+
+	<div class="w-4"></div>
+	<h1 class="text-xl">TODO-Stash</h1>
+	<div class="grow"></div>
+	<LightSwitch />
+</div>
+<Separator />
+<h1>Example Text</h1>
+
+<Sheet.Root>
+	<Sheet.Trigger
+		class="{buttonVariants({ variant: 'outline', size: 'icon' })} absolute bottom-4	right-4"
+	>
+		<Layers />
+	</Sheet.Trigger>
+
+	<Sheet.Content side="right">
+		<Sheet.Header>
+			<Sheet.Title>Stash</Sheet.Title>
+			<Sheet.Description>
+				This action cannot be undone. This will permanently delete your account and remove your data
+				from our servers.
+			</Sheet.Description>
+		</Sheet.Header>
+	</Sheet.Content>
+</Sheet.Root>
